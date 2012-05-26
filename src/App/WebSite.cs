28d71803ec56be06f54bc3205a5ce17200c44cf3
Andrew Walker <alwalker21@live.com>
2012-05-26 18:17:22 -0500

    Added site and application level config values.

--- src/App/WebSite.cs
+++ src/App/WebSite.cs
@@ -3,26 +3,75 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Xml.Linq;
 
 namespace IISExpressManager
 {
     public class WebSite
     {
-        enum Binding {Unknown = 0, HTTP, FTP};
+        enum Binding { Unknown = 0, HTTP, FTP };
 
         private int _id;
-        private string _name;
-        private bool _serverAutoStart;
-        private string _applicationPath;
-        private string _applicationPool; //TODO:Research app pools
         private string _virtualPath;
         private string _physicalPath;
-        private Binding _binding;     
+        private Binding _binding;
         private string _bindingInformation;
+        
+        public int Id { get { return _id; } }
+        public string Name { get; set; }
+        public bool ServerAutoStart { get; set; }
+        public string ApplicationPool { get; set; } //TODO:Research app pools
+        public string ApplicationPath { get; set; }
+
+        protected WebSite(int id, string name, bool serverAutoStart, string applicationPath, string applicationPool)
+        {
+            _id = id;
+            Name = name;
+            ServerAutoStart = serverAutoStart;
+            ApplicationPath = applicationPath;
+            ApplicationPool = applicationPool;
+        }
 
         public static IList<WebSite> GetAllWebsites(IFileIO fileIO)
         {
-            return null;
+            if (fileIO == null)
+            {
+                throw new ArgumentNullException("fileIO");
+            }
+
+            XDocument xdoc = null;
+            try
+            {
+                xdoc = fileIO.GetSitesSection();
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException(
+                    String.Format("Error access IIS Express applicationhost config file: {0}", ex.Message));
+            }
+            if (xdoc == null)
+            {
+                return null;
+            }
+
+            try
+            {
+                var sites =
+                    from site in xdoc.Descendants("site")
+                    select new WebSite(
+                        Convert.ToInt32(site.Attribute("id").Value),
+                        site.Attribute("name").Value,
+                        site.Attribute("serverAutoStart") == null ? true : Convert.ToBoolean(site.Attribute("serverAutoStart").Value),
+                        site.Element("application").Attribute("path").Value,
+                        site.Element("application").Attribute("applicationPool") == null ? string.Empty : site.Element("application").Attribute("applicationPool").Value
+                        );
+                return sites.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException(
+                    String.Format("Error parsing applicationhost config file: {0}", ex.Message));
+            }
         }
     }
 }
--- tests/UnitTests/WebSiteTests.cs
+++ tests/UnitTests/WebSiteTests.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Xunit;
+using Moq;
+using IISExpressManager;
+using System.Xml.Linq;
 
 namespace UnitTests
 {
     public class WebSiteTests
     {
+        private Mock<IFileIO> _fileIOMock = new Mock<IFileIO>();
+        private XDocument _validSites = XDocument.Load("Valid_Sites_Section.xml");
+
+        [Fact]
+        public void TestGetAllWebSite()
+        {
+            _fileIOMock.Setup(x => x.GetSitesSection()).Returns(_validSites);
+
+            var sites = WebSite.GetAllWebsites(_fileIOMock.Object);
+
+            Assert.Equal(4, sites.Count);
+            Assert.Equal("WebSite1", sites[0].Name);
+            Assert.Equal(1, sites[0].Id);
+            Assert.True(sites[0].ServerAutoStart);
+            Assert.Equal("/", sites[0].ApplicationPath);
+            Assert.Equal(string.Empty, sites[0].ApplicationPool);
+            Assert.Equal("Clr4IntegratedAppPool", sites[1].ApplicationPool);
+        }
     }
 }
