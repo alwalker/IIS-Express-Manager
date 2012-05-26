688ab0e9ae92c14776ba5051cb57f13b64adbb39
Andrew Walker <alwalker21@live.com>
2012-05-26 16:47:25 -0500

    Initial project setup.

---
+++ src/App/FileIO.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace IISExpressManager
+{
+    public class FileIO : IFileIO
+    {
+        public FileIO()
+        {
+
+        }
+
+        public XDocument GetSitesSection()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/App/WebSite.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace IISExpressManager
+{
+    public class WebSite
+    {
+        enum Binding {Unknown = 0, HTTP, FTP};
+
+        private int _id;
+        private string _name;
+        private bool _serverAutoStart;
+        private string _applicationPath;
+        private string _applicationPool; //TODO:Research app pools
+        private string _virtualPath;
+        private string _physicalPath;
+        private Binding _binding;     
+        private string _bindingInformation;
+
+        public static IList<WebSite> GetAllWebsites(IFileIO fileIO)
+        {
+            return null;
+        }
+    }
+}
